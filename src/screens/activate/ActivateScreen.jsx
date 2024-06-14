9f720b87a9dc67e752eb6a3e17aef954a196f4b7
Cheikh <115700706+saandiarem@users.noreply.github.com>
2024-06-14 18:19:30 -0400

    update activate screen

--- src/screens/activate/ActivateScreen.jsx
+++ src/screens/activate/ActivateScreen.jsx
@@ -1,11 +1,12 @@
 import AreaactivateTop from '../../components/activate/AreatopActivate/AreaactivateTop';
 import Areaactivate from '../../components/activate/areaActivate/Areaactivate';
-
+import AreaEvaluation from '../../components/activate/AreaEvaluation/AreaEvaluation';
 const ActivateScreen = () => {
   return (
     <div className="content-area">
       <AreaactivateTop />
       <Areaactivate />
+      <AreaEvaluation />
     </div>
   );
 };
