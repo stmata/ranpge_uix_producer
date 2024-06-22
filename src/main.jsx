44d9aa0d14bee00fea9d18b95d9112b04ecde613
stmata <108939392+stmata@users.noreply.github.com>
2024-06-21 23:29:26 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const store = createStore({
   authName:'_auth',
   authType:'cookie',
   cookieDomain: window.location.hostname,
-  cookieSecure: window.location.protocol === 'http:',
+  cookieSecure: window.location.protocol === 'https:',
   refresh : refreshApi
 });
 ReactDOM.createRoot(document.getElementById("root")).render(
